47cb1462fcabbda11d132e9bbf8a789fcb03a296
surekhakallutla <surekhakallutla@gmail.com>
2022-01-31 16:23:08 +0530

    Making Maximum Method as Generic Type

--- src/com/bridgelabz/generics/GenericsMain.java
+++ src/com/bridgelabz/generics/GenericsMain.java
@@ -1,25 +1,18 @@
 package com.bridgelabz.generics;
 
-import java.util.Scanner;
-
 public class GenericsMain {
 	
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		
-		System.out.println("Enter first String Value");
-		String a = scn.next();
-		System.out.println("Enter second String Value");
-		String b = scn.next();
-		System.out.println("Enter third String Value");
-		String c = scn.next();
-		
+				
 		MaxVariable maxVariable = new MaxVariable();
-		display(maxVariable.maximum(a, b, c));
+		
+		display(maxVariable.maximum(10,20,30));
+		display(maxVariable.maximum(76.5732,20.8743,3.141));
+		display(maxVariable.maximum("Apple","Peach","Banana"));
 			
 	}
 	
-	public static void display(String max) {
-		System.out.println("The Maximum Integer is " + max);
+	public static <Generic>void display(Generic max) {
+		System.out.println("The Maximum Variable is " + max);
 	}
 }
--- src/com/bridgelabz/generics/MaxVariable.java
+++ src/com/bridgelabz/generics/MaxVariable.java
@@ -2,36 +2,8 @@ package com.bridgelabz.generics;
 
 public class MaxVariable {
 
-/*
-	public Integer maximum(Integer x, Integer y, Integer z) {
-		Integer max = x;
-		
-		if(y.compareTo(max) > 0)
-			max = y;
-		
-		if(z.compareTo(max) > 0)
-			max = z;
-		
-		return max;	
-	}
-*/
-
-/*
-	public Float maximum(Float x, Float y, Float z) {
-		Float max = x;
-		
-		if(y.compareTo(max) > 0)
-			max = y;
-		
-		if(z.compareTo(max) > 0)
-			max = z;
-		
-		return max;	
-	}
-*/	
-	
-	public String maximum(String x, String y, String z) {
-		String max = x;
+	public <Generic extends Comparable<Generic>> Generic maximum(Generic x, Generic y, Generic z) {
+		Generic max = x;
 		
 		if(y.compareTo(max) > 0)
 			max = y;
