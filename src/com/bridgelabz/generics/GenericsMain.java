cef4353519f205b38a38cfe1ea89716a6257a819
surekhakallutla <surekhakallutla@gmail.com>
2022-01-31 15:01:44 +0530

    Finding Maximum String Variable

--- src/com/bridgelabz/generics/GenericsMain.java
+++ src/com/bridgelabz/generics/GenericsMain.java
@@ -7,19 +7,19 @@ public class GenericsMain {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		
-		System.out.println("Enter first Float Value");
-		Float a = scn.nextFloat();
-		System.out.println("Enter second Float Value");
-		Float b = scn.nextFloat();
-		System.out.println("Enter third Float Value");
-		Float c = scn.nextFloat();
+		System.out.println("Enter first String Value");
+		String a = scn.next();
+		System.out.println("Enter second String Value");
+		String b = scn.next();
+		System.out.println("Enter third String Value");
+		String c = scn.next();
 		
 		MaxVariable maxVariable = new MaxVariable();
 		display(maxVariable.maximum(a, b, c));
 			
 	}
 	
-	public static void display(Float max) {
+	public static void display(String max) {
 		System.out.println("The Maximum Integer is " + max);
 	}
 }
--- src/com/bridgelabz/generics/MaxVariable.java
+++ src/com/bridgelabz/generics/MaxVariable.java
@@ -15,9 +15,23 @@ public class MaxVariable {
 		return max;	
 	}
 */
-	
+
+/*
 	public Float maximum(Float x, Float y, Float z) {
 		Float max = x;
+		
+		if(y.compareTo(max) > 0)
+			max = y;
+		
+		if(z.compareTo(max) > 0)
+			max = z;
+		
+		return max;	
+	}
+*/	
+	
+	public String maximum(String x, String y, String z) {
+		String max = x;
 		
 		if(y.compareTo(max) > 0)
 			max = y;
