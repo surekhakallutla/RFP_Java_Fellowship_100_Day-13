a0d6fbc41511fbc41387f331b376bb0d1e18c6e1
surekhakallutla <surekhakallutla@gmail.com>
2022-02-05 10:38:39 +0530

    Creating generic print method to print different datatype values

--- src/com/bridgelabz/generics/GenericsMain.java
+++ src/com/bridgelabz/generics/GenericsMain.java
@@ -11,13 +11,22 @@ public class GenericsMain {
 		String[] stringArray = {"Apple","Peach","Banana","Mango","Grape"};
 		
 		MaxVariable max = new MaxVariable();
+/*		
 		List<Integer> ints = max.toList(intArray);
 		List<Double> doubles = max.toList(doubleArray);
 		List<String> strings = max.toList(stringArray);
-		
+*/
+/*		
 		System.out.println("The maximum element of three integers is "+max.maxVariable(intArray));
 		System.out.println("The maximum element of three Doubles is "+max.maxVariable(doubleArray));
 		System.out.println("The maximum element of three Strings is "+max.maxVariable(stringArray));
-				
+*/		
+		print(max.maxVariable(intArray));
+		print(max.maxVariable(doubleArray));
+		print(max.maxVariable(stringArray));
+	}
+	
+	public static <Generic> void print(Generic max){
+		System.out.println("The maximum elements of three elements is "+max);		
 	}
 }
